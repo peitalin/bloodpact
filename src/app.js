089e5e2577532c1b8513f367beb94ee4a05afb52
Peita Lin <n6378056@gmail.com>
2016-10-23 01:32:00 +1000

    responsive

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import './app.css';
+import './app.scss';
 import './animation.css';
 
 import Waypoint from 'react-waypoint';
@@ -9,10 +9,11 @@ import * as snapsvg from 'snapsvg-cjs';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 
+
 class SVGFloater extends React.Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             message: ""
         }
@@ -21,7 +22,8 @@ class SVGFloater extends React.Component {
     _renderSVG() {
         switch (this.state.message) {
             case "IN":
-                return <ReactCSSTransitionGroup transitionName='svgFloater'
+                return <ReactCSSTransitionGroup
+                            transitionName='svgFloater'
                             transitionAppear={true}
                             transitionAppearTimeout={500}
                             transitionLeaveTimeout={500}
@@ -41,18 +43,16 @@ class SVGFloater extends React.Component {
 
     render() {
         return (
-            <div style={{height: '600px'}}>
-                <div className="waypoint-line">Waypoint Boundary
+            <div>
+				<div className="waypoint-line"><hr/>Waypoint: Line
                     <Waypoint
                         onLeave={this._setMessage.bind(this, 'OUT')}
-                        topOffset={'-2000px'} />
-                </div>
-                <div className="spacer">a</div>
-                <div className="waypoint-line">Waypoint Boundary
+                        topOffset={-1200} />
+                <div className="spacer" style={{lineHeight: "50px"}}><hr/></div>
                     <Waypoint
                         onEnter={this._setMessage.bind(this, 'IN')}
-                        topOffset={'-1000px'}
-                        bottomOffset={'200px'} />
+                        topOffset={-1000}
+                        bottomOffset={200} />
                     {this._renderSVG()}
                 </div>
             </div>
@@ -67,24 +67,84 @@ class App extends React.Component {
 
     constructor(props) {
         super();
+        this.state = {
+            scroll: 0
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    handleScroll(event) {
+        let scrollTop = event.srcElement.body.scrollTop
+        this.setState({
+            scroll: event.srcElement.body.scrollTop
+        })
+        console.log(`Scrolling: ${this.state.scroll}`);
     }
 
     render() {
         return (
-            <div className="container">
-                <h1>BloodPact</h1>
+            <div>
+                <img src="./img/perfusion2.png"/>
+
+                <h1>BloodPact: Blood-Backed Health Insurance</h1>
+
+				<div>
+                    <h1>Donate Blood and gift insurance to Family and Friends.</h1>
+                </div>
+
+                <div className='container'>
+                    <div className="svgContainer">
+                        <SVGFloater svgSrc="./img/blooddrop2.svg"/>
+                    </div>
+                    <div className="textBox">
+                        <p>1) increase aggregate levels of blood donation
+                            (can't just pay for blood since that's immoral and can
+                            actually decrease levels of donation)</p>
+                    </div>
+                </div>
+
                 <div className="spacer">a</div>
+
+                <div className='container'>
+                    <div className="svgContainer">
+                        <SVGFloater svgSrc="./img/perfusion.svg"/>
+                    </div>
+                    <div className="textBox">
+                        <p>2) Solves the "adverse selection problem" in health insurance:
+                            people conceal information about their health status and their habits
+                            (smoking, diet) in order to obtain cheaper premiums.</p>
+                    </div>
+                </div>
                 <div className="spacer">a</div>
+                <div className='container'>
+                    <div className="svgContainer">
+                        <SVGFloater svgSrc="./img/finger.svg"/>
+                    </div>
+                    <div className="textBox">
+                        <p>3) By donating blood we can do blood tests and screen doners for
+                            viable “blood-pact” candidates. This reveals information about
+                            their diet, habits and actual state of health.</p>
+                    </div>
+                </div>
                 <div className="spacer">a</div>
+                <div className="svgContainer">
+                    <SVGFloater svgSrc="./img/bloodvial_many.svg"/>
+                </div>
                 <div className="spacer">a</div>
-                <SVGFloater svgSrc="./img/bloodvial_many.svg"/>
+                <div className="svgContainer">
+                    <SVGFloater svgSrc="./img/bloodpack.svg"/>
+                </div>
                 <div className="spacer">a</div>
-                <div className="spacer">a</div>
-                <SVGFloater svgSrc="./img/bloodpack.svg"/>
-                <div className="spacer">a</div>
-                <div className="spacer">a</div>
-                <SVGFloater svgSrc="./img/doctor_she.svg"/>
-                <div className="spacer">a</div>
+                <div className="svgContainer">
+                    <SVGFloater svgSrc="./img/doctor_she.svg"/>
+                </div>
                 <div className="spacer">a</div>
             </div>
         );
