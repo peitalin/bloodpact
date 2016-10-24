b22af275b25195edbc4ce2e6f71d7713da889f23
Peita Lin <n6378056@gmail.com>
2016-10-25 00:43:02 +1000

    scroll performance enhancements

--- src/app.js
+++ src/app.js
@@ -66,12 +66,33 @@ class App extends React.Component {
         super();
         this.state = {
             scrollTop: 0,
-            lastRotationValue: 0
+            elems: {}
         }
     }
 
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll.bind(this));
+
+        this.setState({
+            elems: {
+                logo1: document.getElementById('logo1'),
+                logo2: document.getElementById('logo2'),
+                logo3: document.getElementById('logo3'),
+
+                dither1: document.getElementById('parallaxDither1'),
+                dither2: document.getElementById('parallaxDither2'),
+                dither3: document.getElementById('parallaxDither3'),
+
+                forewoman: document.getElementById('fore-woman'),
+                foreman1: document.getElementById('fore-man1'),
+                foreman2: document.getElementById('fore-man2'),
+
+                fixedContainer1: document.getElementById('fixedContainer1'),
+                fixedContainer2: document.getElementById('fixedContainer2'),
+                fixedContainer3: document.getElementById('fixedContainer3'),
+            }
+        })
+
     }
 
     componentWillUnmount() {
@@ -79,77 +100,88 @@ class App extends React.Component {
     }
 
     handleScroll(event) {
-        let bodyHeight = document.body.scrollHeight
+
         this.setState({
             scrollTop: event.srcElement.body.scrollTop
         })
         let scrollTop = this.state.scrollTop
+        let elems = this.state.elems
+        // console.log(`Scrolling: ${this.state.scrollTop}`);
 
-        let dither = document.getElementById('parallaxDither')
-        dither.style.opacity = `${scrollTop/400}`
+        // move document.getElementById('') to componentDidMount for performance
+        // so that it won't lookup elemens every time there is a scroll event
+        let logo1 = elems.logo1
+        let logo2 = elems.logo2
+        let logo3 = elems.logo3
+        let dither1 = elems.dither1
+        let dither2 = elems.dither2
+        let dither3 = elems.dither3
+        let forewoman = elems.forewoman
+        let foreman1 = elems.foreman1
+        let foreman2 = elems.foreman2
+        let fixedContainer1 = elems.fixedContainer1
+        let fixedContainer2 = elems.fixedContainer2
+        let fixedContainer3 = elems.fixedContainer3
 
-        console.log(`Scrolling: ${this.state.scrollTop}`);
 
-        let logo = document.getElementById('logo1')
-        logo.style.transform = `translate(0px, ${scrollTop/2}%)`
 
-        let logo2 = document.getElementById('logo2')
-        if (scrollTop >= window.innerHeight) {
-            logo2.style.transform = `translate(0px, ${(scrollTop-window.innerHeight)/2}%)`
-        }
+        // parallax
+        logo1.style.transform = `translate(0px, ${scrollTop/2}%)`
+        dither1.style.opacity = `${scrollTop/400}`
 
-        let logo3 = document.getElementById('logo3')
-        if (scrollTop >= (window.innerHeight + 100)*2) {
-            logo3.style.transform = `translate(0px, ${(scrollTop-(window.innerHeight + 100)*2)/2}%)`
-        }
-
-        let foreman = document.getElementById('fore-man')
-        let foreman2 = document.getElementById('fore-man2')
-        let forewoman = document.getElementById('fore-woman')
-
-        if (scrollTop > 5) {
-            foreman.style.transform = `translate(${scrollTop/(2*(1 + (5-scrollTop)/1000))}%, 0px)`
+        if ( scrollTop >= window.innerHeight ) {
+            let scale2 = scrollTop - window.innerHeight
+            logo2.style.transform = `translate(0px, ${scale2/2}%)`
+            dither2.style.opacity = `${scale2/400}`
         } else {
-            foreman.style.transform = `translate(${scrollTop/2}%, 0px)`
+            dither2.style.opacity = `0`
         }
-        if (scrollTop > 5) {
-            foreman2.style.transform = `translate(${scrollTop/(3*(1 + (5-scrollTop)/1200))}%, 0px)`
+
+        if ( scrollTop >= (window.innerHeight + 100)*2 ) {
+            let scale3 = scrollTop - (window.innerHeight + 100) * 2
+            logo3.style.transform = `translate(0px, ${scale3/2}%)`
+            dither3.style.opacity = `${scale3/400}`
         } else {
-            foreman2.style.transform = `translate(${scrollTop/3}%, 0px)`
+            dither3.style.opacity = `0`
         }
-        forewoman.style.transform = `translate(${scrollTop/4}%, 0px)`
 
 
+        // people sliders
+        if (scrollTop < window.innerHeight) {
+            forewoman.style.transform = `translate(${scrollTop/4}%, 0px)`
+            foreman1.style.transform = `translate(${scrollTop/(3*(1 + (5-scrollTop)/1200))}%, 0px)`
+            foreman2.style.transform = `translate(${scrollTop/(2*(1 + (5-scrollTop)/1000))}%, 0px)`
+        }
+
+
+
+        //// THRESHOLDS FOR Fixed containers/placeholders
+        // Must be here, otherwise breaks on window resize
+        // window-height * 2 plus 100px for the placeholder
+        let threshold1 = window.innerHeight*2 + 100
+        // window height * 3 plus 200px for the 2 placeholders
+        let threshold2 = window.innerHeight*3 + 2*100
+        // window height * 4 plus 300px for the 3 placeholders
+        let threshold3 = window.innerHeight*4 + 3*100
 
         // 1st fixed container
-        let fixedContainer = document.getElementById('fixedContainer')
-        if ((window.innerHeight <= scrollTop) && (scrollTop <= window.innerHeight*2+100)) {
-            fixedContainer.style.position = 'fixed'
+        if (window.innerHeight <= scrollTop && scrollTop <= threshold1) {
+            fixedContainer1.style.position = 'fixed'
         } else {
-            fixedContainer.style.position = 'relative'
+            fixedContainer1.style.position = 'relative'
         }
-
         // 2nd fixed container
-        let fixedContainer2 = document.getElementById('fixedContainer2')
-        let lowerThreshold2 = window.innerHeight*2 + 100
-        if ((lowerThreshold2 <= scrollTop) && (scrollTop <= window.innerHeight*3+2*100)) {
+        if (threshold1 <= scrollTop && scrollTop <= threshold2) {
             fixedContainer2.style.position = 'fixed'
         } else {
             fixedContainer2.style.position = 'relative'
         }
-
         // 3rd fixed container
-        let fixedContainer3 = document.getElementById('fixedContainer3')
-        let lowerThreshold3 = window.innerHeight*3 + 2*100
-        if ((lowerThreshold3 <= scrollTop) && (scrollTop <= window.innerHeight*4+3*100)) {
+        if (threshold2 <= scrollTop && scrollTop <= threshold3) {
             fixedContainer3.style.position = 'fixed'
         } else {
             fixedContainer3.style.position = 'relative'
         }
-
-
-
-
 
 
         // Heart animation
@@ -157,18 +189,18 @@ class App extends React.Component {
         let baserate = 1/1000 // 600 scroll for 1 full rotation
         // start decay 150 scroll into the page
         let decayrate = Math.exp(-(scrollTop-150)/800)
-
-        this.setState({ lastRotationValue: scrollTop * baserate * decayrate })
+        let lastRotationValue = scrollTop * baserate * decayrate
 
         if (scrollTop <= 650) {
             heart.style.transform = `
                 translate(0px, ${scrollTop/4}%)
-                rotate(${0.65 + this.state.lastRotationValue}turn)
+                rotate(${0.65 + lastRotationValue}turn)
             `
-        } else {
+        }
+        if (scrollTop > 650 && scrollTop < window.innerHeight*2) {
             heart.style.transform = `
             translate(0px, ${scrollTop/(4 / (1+(scrollTop - 650)/2000))}%)
-                rotate(${0.65 + this.state.lastRotationValue}turn)
+                rotate(${0.65 + lastRotationValue}turn)
             `
         }
 
@@ -179,7 +211,7 @@ class App extends React.Component {
         let scrollTop = this.state.scrollTop
         let lowerThreshold = threshold*n + (n-1)*100
 
-        if ((lowerThreshold <= scrollTop) && (scrollTop <= threshold*(n+1)+n*100)) {
+        if (lowerThreshold <= scrollTop && scrollTop <= threshold*(n+1) + n*100) {
             return <div id='tempPlaceholder' className='container'>
                     <div className="textBox">placeholder</div>
                 </div>
@@ -194,9 +226,9 @@ class App extends React.Component {
             <div>
 
                 <div className='parallax' id="parallaxBox1">
-                    <div id="parallaxDither"></div>
-                    <img className='people' id="fore-man" src={require('./img/doctor_he2.svg')} />
-                    <img className='people' id="fore-man2" src={require('./img/doctor_he4.svg')} />
+                    <div className='dither' id="parallaxDither1"></div>
+                    <img className='people' id="fore-man2" src={require('./img/doctor_he2.svg')} />
+                    <img className='people' id="fore-man1" src={require('./img/doctor_he4.svg')} />
                     <img className='people' id="fore-woman" src={require('./img/doctor_she.svg')} />
                     <div className='logo' id="logo1">
                         <h1>Bloodpact: Blood-Backed Health Insurance</h1>
@@ -205,7 +237,7 @@ class App extends React.Component {
 
 
                 {this._placeholder(window.innerHeight, 1)}
-                <div id='fixedContainer' className='container'>
+                <div id='fixedContainer1' className='container'>
                     <SVGFloater svgSrc={require('./img/blooddrop2.svg')} />
                     <div className="textBox">
                         1) Increase aggregate levels of blood donation
@@ -215,7 +247,7 @@ class App extends React.Component {
                 </div>
 
                 <div className='parallax' id="parallaxBox2">
-                    <div id="parallaxDither2"></div>
+                    <div className='dither' id="parallaxDither2"></div>
                     <img id="heart-beat" src={require("./img/heart.svg")} />
                     <div className='logo' id="logo2">
                         <h1>Give Blood and Gift Insurance to Family and Friends.</h1>
@@ -235,6 +267,7 @@ class App extends React.Component {
 
 
                 <div className='parallax' id="parallaxBox3">
+                    <div className='dither' id="parallaxDither3"></div>
                     <div className='logo' id="logo3">
                         <h1>One pint saves three lives</h1>
                     </div>
